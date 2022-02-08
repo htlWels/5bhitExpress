4faad734ed1dc59c2593a14604b9f6eedb93a1e6
refr <franz.reitinger@htl-wels.at>
2022-02-08 20:12:05 +0100

    bug fixes

--- config/passport.js
+++ config/passport.js
@@ -1,39 +1,42 @@
-var LocalStrategy = require ('passport-local').Strategy
+var LocalStrategy = require('passport-local').Strategy
 const User = require('../models/userSchema')
 const passport = require('passport')
 
-passport.serializeUser((user,done) =>{
-    done(null,user.id)
+passport.serializeUser((user, done) => {
+    done(null, user.id)
 })
 
-passport.deserializeUser((id,done)=>{
-    User.findById(id,(err,user)=>{
-        done(err,user)
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
     })
 })
 
-passport.use('local',new LocalStrategy(
-    {
-        usernameField:'user',
-        passwordField:'password'
+passport.use('local', new LocalStrategy({
+        usernameField: 'user',
+        passwordField: 'password'
     },
-    (user,password,done)=>{
+    (user, password, done) => {
         console.log("Password: search for user: " + user)
-        User.findOne({username:user},(err,userMongo)=>{
+        User.findOne({ username: user }, (err, userMongo) => {
             if (err) {
                 console.log("Mongoose: search user returned an error: " + err)
-                 return done(err)
+                return done(err)
             }
             if (!userMongo) {
-                console.log("Mongoose: User " +  user + " nort found")
-                return done(null,null,{message: 'User not found'})
+                console.log("Mongoose: User " + user + " not found")
+                return done(null, null, { message: 'User not found' })
             }
-            userMongo.validatePassword(password,(err,result)=>{
+            userMongo.validatePassword(password, (err, result) => {
                 if (err)
                     return done(err)
-                done(null,result)
+                if (result)
+                    done(null, userMongo, { message: 'Password is valid' })
+                else {
+                    done(null, null, { message: 'Password not valid' })
+                }
             })
-            
+
         })
 
     }
--- routes/index.js
+++ routes/index.js
@@ -4,48 +4,50 @@ const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 
-router.post('/login',(req,res,next)=>{
-  console.log('/login ....')
-  passport.authenticate('local',{session:true},(err,user,info)=>{
-    let jsonResult = {
-      error:false,
-      userKnown:false,
-      passwordOK:false
-    }
-    if (err) 
-      jsonResult.error=true
-    else if (!user) {  // not null
-      jsonResult.userKnown=false
-    }
-    else if (user == false) // boolean, wrong password
-        jsonResult.passwordOK = false
-    else {
-      jsonResult.passwordOK = true
-      jsonResult.userKnown =true
-    }
-    res.setHeader('Content-Type','application/json')
-    res.end(JSON.stringify(jsonResult))
-      
+router.post('/login', (req, res, next) => {
+    console.log('/login ....')
+    passport.authenticate('local', { session: true }, (err, user, info) => {
+        let jsonResult = {
+            error: false,
+            userKnown: false,
+            passwordOK: false
+        }
+        if (err)
+            jsonResult.error = true
+        else if (!user) {
+            if (info.message === "Password not valid") {
+                jsonResult.userKnown = true
+            }
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(jsonResult))
+        } else {
+            req.login(user, (error) => {
+                if (error) return next(error);
+                console.log("Request Login supossedly successful.");
+                res.setHeader('Content-Type', 'application/json');
+                jsonResult.passwordOK = true
+                jsonResult.userKnown = true
+                return res.end(JSON.stringify(jsonResult)) // user  known
+            })
 
+        }
 
-    
-  })
+    })(req, res, next)
 })
 
-router.get('/sessions', (req,res,next)=>{
-  if (req.session.page_views) {
-    req.session.page_views++
-    res.send("You visited this page " + req.session.page_views + " times.")
-  } else {
-    req.session.page_views = 1
-    res.send("Welcome to this page for the first time")
-  }
+router.get('/sessions', (req, res, next) => {
+    if (req.session.page_views) {
+        req.session.page_views++
+            res.send("You visited this page " + req.session.page_views + " times.")
+    } else {
+        req.session.page_views = 1
+        res.send("Welcome to this page for the first time")
+    }
 })
 
 
 module.exports = router;
- 
